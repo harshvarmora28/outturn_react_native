b1ec81bb6b812b4649734e16dd6b2115c12afcc2
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-11-30 18:36:33 +0530

    done with bottom tab navigator

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -5,7 +5,8 @@ import {
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 import { Home, Portfolio, Market, Profile } from "../screens"
-import { COLORS } from "../constants"
+import { COLORS, icons } from "../constants"
+import { TabIcon } from "../components";
 
 const Tab = createBottomTabNavigator()
 
@@ -14,7 +15,9 @@ const Tabs = () => {
     return (
         <Tab.Navigator
             tabBarOptions={{
+                showLabel: false,
                 style: {
+                    height: 140,
                     backgroundColor: COLORS.primary,
                     borderTopColor: "transparent",
                 }
@@ -23,22 +26,78 @@ const Tabs = () => {
             <Tab.Screen
                 name="Home"
                 component={Home}
+                options={{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcon
+                            focused={focused}
+                            icon={icons.home}
+                            label="Home"
+                            />
+                        )
+                    }
+                }}
             />
             <Tab.Screen
                 name="Portfolio"
                 component={Portfolio}
+                options={{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcon
+                            focused={focused}
+                            icon={icons.briefcase}
+                            label="Portfolio"
+                            />
+                        )
+                    }
+                }}
             />
             <Tab.Screen
                 name="Trade"
                 component={Home}
+                options={{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcon
+                            focused={focused}
+                            icon={icons.trade}
+                            label="Trade"
+                            isTrade={true}
+                            />
+                        )
+                    }
+                }}
             />
             <Tab.Screen
                 name="Market"
                 component={Market}
+                options={{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcon
+                            focused={focused}
+                            icon={icons.market}
+                            label="Market"
+                            />
+                        )
+                    }
+                }}
             />
             <Tab.Screen
                 name="Profile"
                 component={Profile}
+                options={{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcon
+                            focused={focused}
+                            icon={icons.profile}
+                            label="Profile"
+                            />
+                        )
+                    }
+                }}
             />
         </Tab.Navigator>
     )
